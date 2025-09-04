a1b8706fd04df4e10df2e12c1f95ad3ef64e7403
pouyan nazarzade <poyannazarzade@gmail.com>
2025-09-04 17:07:35 -0400

    added the who am I section

--- Desktop/New portfolio/portfolio/src/App.jsx
+++ Desktop/New portfolio/portfolio/src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './index.css'
 import Hero from './components/Hero'
+import WhoAmI from './components/WhoAmI'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 function App() {
 
   return (
-    <>
+    <div>
        <Hero/>
-    </>
+       <WhoAmI/>
+    </div>
   )
 }
 
