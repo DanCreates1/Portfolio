c3d168bbba365bef43c99abd93d31ca54ef044be
pouyan nazarzade <poyannazarzade@gmail.com>
2025-09-09 17:27:57 -0400

    working on the projects section

--- Desktop/New portfolio/portfolio/src/components/projects.jsx
+++ Desktop/New portfolio/portfolio/src/components/projects.jsx
@@ -4,6 +4,21 @@ export default function Projects() {
         <div className="Projects">
           <h1>My Projects</h1>
           </div>
+
+          <div className="projectCards">
+           
+            <div className="card">
+              <img src="" alt="" />
+              <h2>Project 1</h2>
+              <p>Description of Project 1</p>
+              
+              // use the img below for the tech stuff used to build it
+              <img src="" alt="" />
+            </div>
+
+    
+
+          </div>
         </section>
     )
 
