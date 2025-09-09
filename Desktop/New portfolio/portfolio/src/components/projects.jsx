6e2fb26b08723c8e941c0613782f86bcca39b74d
pouyan nazarzade <poyannazarzade@gmail.com>
2025-09-09 17:19:40 -0400

    working on the projects section

--- Desktop/New portfolio/portfolio/src/components/projects.jsx
+++ Desktop/New portfolio/portfolio/src/components/projects.jsx
@@ -2,10 +2,7 @@ export default function Projects() {
     return(
         <section id="Projects">
         <div className="Projects">
-          <h1>My <span>Projects</span></h1>
-          <p>
-            Here are some of the projects I've worked on recently. These projects showcase my skills in web development, problem-solving, and creativity. Each project is a reflection of my passion for coding and my commitment to continuous learning.
-          </p>
+          <h1>My Projects</h1>
           </div>
         </section>
     )
