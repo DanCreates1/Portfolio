c71155657e9e2bbdc4d5107e308fed1dbb41f6d5
pouyan nazarzade <poyannazarzade@gmail.com>
2025-12-02 17:23:34 -0500

    Updated the projects section

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -70,21 +70,21 @@ export default function Projects() {
 
 
 
-<div className="card">
-              <img src=" /projectsImg/portfolioV2.png" alt=""  />
-              <h2>Dusic</h2>
-              <p>
-                Dusic is a lightweight web tool that displays your current Spotify track in
-                real time, designed to make sharing what you’re listening to simple and stylish.
-              </p>
-               <div className="techStack">
-              <i class="fa-brands fa-react"  style={{ color: "cyan" }}></i>
-                  <i class="fa-brands fa-square-js" style={{ color: "yellow" }}></i>
-                  <i class="fa-brands fa-css3-alt" style={{ color: "#17f3ff" }}></i>
-                  <i class="fa-brands fa-html5" style={{ color: "#e34c26" }}></i>
-                  <i class="fa-brands fa-npm" style={{ color: "#CC3534" }}></i>
-                  </div>
-              <a href="https://github.com/DanCreates1/Dusic"  target="_blank"><button>View Project</button></a>
+            <div className="card">
+                <img src=" /projectsImg/portfolioV2.png" alt=""  />
+                <h2>Portfolio V2</h2>
+                <p>
+                  A redesigned and self-made portfolio built to improve layout, 
+                  performance, and modern styling while showcasing my latest projects
+                </p>
+                <div className="techStack">
+                <i class="fa-brands fa-react"  style={{ color: "cyan" }}></i>
+                    <i class="fa-brands fa-square-js" style={{ color: "yellow" }}></i>
+                    <i class="fa-brands fa-css3-alt" style={{ color: "#17f3ff" }}></i>
+                    <i class="fa-brands fa-html5" style={{ color: "#e34c26" }}></i>
+                    <i class="fa-brands fa-npm" style={{ color: "#CC3534" }}></i>
+                    </div>
+                <a href="https://dancreates1.github.io/Dan-Creates/"  target="_blank"><button>View Project</button></a>
             </div>
 
 
