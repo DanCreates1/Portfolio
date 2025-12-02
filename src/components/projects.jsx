3a1509fb914944c4bd5ea55db4d872ef5361f2b2
pouyan nazarzade <poyannazarzade@gmail.com>
2025-12-02 16:55:15 -0500

    Updated the projects section

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -16,26 +16,24 @@ export default function Projects() {
                   <i class="fa-brands fa-square-js" style={{ color: "yellow" }}></i>
                   <i class="fa-brands fa-css3-alt" style={{ color: "#17f3ff" }}></i>
                   <i class="fa-brands fa-html5" style={{ color: "#e34c26" }}></i>
-                  <img src="tailwindCssLogo.png" alt="tailwind Css Logo" />
+                  <img src="/ProgrammingIcons/tailwindCssLogo.png" alt="tailwind Css Logo" />
                   <i class="fa-brands fa-npm" style={{ color: "#CC3534" }}></i>
 
               </div>
               <a href="https://www.codeva.xyz"  target="_blank"><button>View Project</button></a>
             </div>
+            
+            
 
             <div className="card">
-              <img src=" /projectsImg/cars.png" alt=""  />
-              <h2>Cars</h2>
-              <p>This was my frst React project which demonstrate my early React skills,
-                  I made my own Data set for the Cars and soon I will realse it's own unique API
+              <img src=" /projectsImg/ASL.png" alt=""  />
+              <h2>AI Sign Language Detector</h2>
+              <p>
+                A self-trained and fully built AI tool that detects sign language letters
+                using live hand-tracking, created to explore computer vision and machine learning.
               </p>
               <div className="techStack">
-              <i class="fa-brands fa-react"  style={{ color: "cyan" }}></i>
-                  <i class="fa-brands fa-square-js" style={{ color: "yellow" }}></i>
-                  <i class="fa-brands fa-css3-alt" style={{ color: "#17f3ff" }}></i>
-                  <i class="fa-brands fa-html5" style={{ color: "#e34c26" }}></i>
-                  <img src="tailwindCssLogo.png" alt="tailwind Css Logo" />
-                  <i class="fa-brands fa-npm" style={{ color: "#CC3534" }}></i>
+                  <img src="/ProgrammingIcons/python.png" alt="tailwind Css Logo" />
                   </div>
               <a href="https://cars-phi-ten.vercel.app"  target="_blank"><button>View Project</button></a>
             </div>
@@ -57,7 +55,7 @@ export default function Projects() {
 
              <div className="card">
               <img src=" /projectsImg/oldPortfolio.png" alt=""  />
-              <h2>First portfolio</h2>
+              <h2>Portfolio V1</h2>
               <p>
                 My first ever portfolio website, where I learned the
                 basics of web design, layout, and responsive pages.
@@ -69,6 +67,26 @@ export default function Projects() {
                   </div>
               <a href="https://dancreates1.github.io/Pouyan"  target="_blank"><button>View Project</button></a>
             </div>
+
+
+
+<div className="card">
+              <img src=" /projectsImg/portfolioV2.png" alt=""  />
+              <h2>Dusic</h2>
+              <p>
+                Dusic is a lightweight web tool that displays your current Spotify track in
+                real time, designed to make sharing what you’re listening to simple and stylish.
+              </p>
+               <div className="techStack">
+              <i class="fa-brands fa-react"  style={{ color: "cyan" }}></i>
+                  <i class="fa-brands fa-square-js" style={{ color: "yellow" }}></i>
+                  <i class="fa-brands fa-css3-alt" style={{ color: "#17f3ff" }}></i>
+                  <i class="fa-brands fa-html5" style={{ color: "#e34c26" }}></i>
+                  <i class="fa-brands fa-npm" style={{ color: "#CC3534" }}></i>
+                  </div>
+              <a href="https://github.com/DanCreates1/Dusic"  target="_blank"><button>View Project</button></a>
+            </div>
+
 
       <div className="card">
               <img src=" /projectsImg/Dusic.png" alt=""  />
