7b116487ff4e17494f876191752c097348f6ddae
pouyan nazarzade <poyannazarzade@gmail.com>
2025-12-23 23:37:17 -0500

    push, fix the who am I section

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ function App() {
     <div>
        <Hero/>
        <WhoAmI/>
-      <RubiksCube/>
        <Projects/>
       <Contact/>
     </div>
--- src/components/WhoAmI.jsx
+++ src/components/WhoAmI.jsx
@@ -1,18 +1,19 @@
+import RubiksCube from "./RubiksCube.jsx";
 export default function WhoAmI() {
-   console.log(import.meta.env.VITE_EMAILJS_SERVICE_ID);
-
    return (
       
-      <section id="WhoAmI">
-    <div className="WhoAmI">
-      <h1>Who Am <span>I</span>?</h1>
-      <p>
-        Grade 10 student in the IB program with a focus on coding, fitness, and continuous learning. 
-        I enjoy building web applications that are practical and easy to use, while developing skills in web development through self-directed projects.
-        Outside of coding, I'm training to become a certified lifeguard, working out regularly, and exploring new technologies.
-        This portfolio highlights ongoing work and progress in both academics and personal development.
-       </p>
-      </div>
-      </section>
+   <section id="WhoAmI">
+         <div className="CubeAndWhoAmI">
+            <div>Who Am <span>I</span>?</div>
+               <RubiksCube/>
+            </div>
+            <div classNamre="WhoAmIText">
+               Grade 10 student in the IB program with a focus on coding, fitness, and continuous learning. 
+               I enjoy building web applications that are practical and easy to use, while developing skills
+               in web development through self-directed projects. Outside of coding, I'm training to become
+               a certified lifeguard, working out regularly, and exploring new technologies.This portfolio
+               highlights ongoing work and progress in both academics and personal development.
+            </div>
+   </section>
    )
 }
